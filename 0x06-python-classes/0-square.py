c73f4e877848dc75503b77eab94d96e02591010f
Peace Chimamaka <peaceamaka111@gmail.com>
2024-01-24 16:25:15 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' A module that defines an empty class'''
+
+
 class Square:
     ''' An empty class that defines a square'''
     pass
-
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 ''' A modoule that prints a sqare'''
+
+
 class Square:
     '''A class that prints a square'''
     def __init__(self, size):
