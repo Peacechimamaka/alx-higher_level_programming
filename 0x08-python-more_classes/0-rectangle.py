b853e6413e366fc0f5be1314427298d9d3ef0e21
Peace Chimamaka <peaceamaka111@gmail.com>
2024-01-29 23:58:00 -0800

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+'''A module that prints a rectangle
+'''
+
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''A module that prints a rectangle
+'''
+
+
+class Rectangle:
+    '''A class attribute'''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    '''Methods'''
+
+    def width(self):
+        return value
+    def width(self, value):
+        self.value = value
+        if not isinstance(int, value):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("height must be >= 0")
+
+    def height(self):
+        return value
+    def height(self, value):
+        self.value == value
+        if not isinstance(int, value):
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
