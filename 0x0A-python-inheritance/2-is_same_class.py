d2e3b07865a946d1526409b0c6e608e3e97ce6e5
Peacechimamaka <peaceamaka111@gmail.com>
2024-02-09 00:09:58 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''A module that checks for the type of an instance
+'''
+
+
+def is_same_class(obj, a_class):
+    '''A function that prints the type of an instance
+    '''
+    return type(obj) is a_class
