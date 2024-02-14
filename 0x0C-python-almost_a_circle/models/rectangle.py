b793975e87acd846fe7c9f138a69e76ae31a9c15
Peace Chimamaka <peaceamaka111@gmail.com>
2024-02-14 04:00:05 -0800

    Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
+from models.base import Base
+
 '''contains a class rectangle that inherits from base
 '''
-
-
-class Base:
-    '''A class base
-    '''
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        '''the init function
-        '''
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
 
 
 class Rectangle(Base):
