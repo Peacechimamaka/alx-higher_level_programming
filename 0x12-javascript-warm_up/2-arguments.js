973d4168e3ac5bb1fbe6c1f631a74b768fdf9b73
Peacechimamaka <peaceamaka111@gmail.com>
2024-04-10 02:20:34 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const proc = process.argv;
+
+if (proc.length < 3){
+  console.log('No arguement');
+}
+else if ( proc.length === 3){
+  console.log('Arguement found');
+}
+else{
+  console.log('Arguements found');
+}
