9c314afd241ccebf247d00669959d60038277199
Peacechimamaka <peaceamaka111@gmail.com>
2024-05-03 20:12:57 +0100

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2).map(Number)
+  args.sort((a, b) => a-b);
+
+console.log(args[args.length - 2]);
+}
