677af1218f72053b4e1ac506f11466e8b54ba3dc
Peacechimamaka <peaceamaka111@gmail.com>
2024-05-03 15:40:52 +0100

    8-square.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const proc = process.argv[2];
 if (Number(proc)) {
   console.log(`My number: ${Number(proc)}`);
 } else {
-	console.log('Not a number')
+  console.log('Not a number');
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 
 const proc = process.argv;
-const checked = parseInt(proc[2]);
+const checked = Number(proc[2]);
 
-if (isNaN(checked)) {
-	console.log('Missing size');
+if (!checked) {
+  console.log('Missing size');
 } else {
-	let count = checked;
-	while(count > 0) {
-		console.log('X'.repeat(checked))
-		count--;
-	}
+  let count = checked;
+
+  while (count > 0) {
+    console.log('X'.repeat(checked));
+    count--;
+  }
 }
