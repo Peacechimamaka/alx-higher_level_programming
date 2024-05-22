fcacadbb0ae79c8685390a890b554a53eafebb0c
Peacechimamaka <peaceamaka111@gmail.com>
2024-05-22 22:24:53 +0100

    Task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-//script that reads and prins the content of a file.
+// script that reads and prins the content of a file.
 
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (error, content) {
-	console.log(error || content);
+  console.log(error || content);
 });
